1f2b9cf42faef7024c358232828f0af64f8500b8
jntakpe <j.ntakpe@gmail.com>
2017-06-12 21:11:28 +0200

    Spring Initializer generation

---
+++ src/main/kotlin/com/github/jntakpe/devsskills/DevsSkillsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jntakpe.devsskills
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevsSkillsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DevsSkillsApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/github/jntakpe/devsskills/DevsSkillsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.jntakpe.devsskills
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DevsSkillsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
