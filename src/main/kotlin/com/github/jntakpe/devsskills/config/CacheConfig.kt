a60eb99c235786914ab49c11ee7dbc1bf92cf428
jntakpe <j.ntakpe@gmail.com>
2017-07-31 22:06:50 +0200

    Update Spring Boot version

--- src/main/kotlin/com/github/jntakpe/devsskills/config/CacheConfig.kt
+++ src/main/kotlin/com/github/jntakpe/devsskills/config/CacheConfig.kt
@@ -1,34 +1,32 @@
 package com.github.jntakpe.devsskills.config
 
 import com.github.jntakpe.devsskills.config.properties.RedisCacheProperties
-import com.github.jntakpe.devsskills.config.properties.RedisCacheProperties.Companion.DEFAULT_EXPIRY
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.cache.RedisCacheWriter
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 import java.time.Duration
+import java.time.Duration.ofHours
+import java.time.Duration.ofSeconds
 
 @Configuration
 class CacheConfig(private val cacheProperties: RedisCacheProperties) {
 
     @Bean
     fun redisConnectionFactory() = LettuceConnectionFactory(RedisStandaloneConfiguration("localhost", 6379),
-            LettuceClientConfiguration.builder().shutdownTimeout(Duration.ofSeconds(1)).build())
+            LettuceClientConfiguration.builder().shutdownTimeout(ofSeconds(1)).build())
 
     @Bean
-    fun redisTemplate() = RedisTemplate<String, Any>().apply { connectionFactory = redisConnectionFactory() }
+    fun cacheManager() = RedisCacheManager(redisCacheWriter(), defaultCacheConfig(), buildCachesConfig())
 
-    @Bean
-    fun cacheManager() = RedisCacheManager(redisTemplate()).apply { configureCacheManager() }
+    private fun defaultCacheConfig(duration: Duration = ofHours(1)) = RedisCacheConfiguration.defaultCacheConfig().entryTtl(duration)
 
-    private fun RedisCacheManager.configureCacheManager() {
-        setDefaultExpiration(DEFAULT_EXPIRY)
-        setUsePrefix(true)
-        cacheNames = cacheProperties.caches.map(RedisCacheProperties.RedisCache::name)
-        setExpires(cacheProperties.caches.associateBy({ it.name }, { it.expiry }))
-    }
+    private fun redisCacheWriter() = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory())
+
+    private fun buildCachesConfig() = cacheProperties.caches.associateBy({ it.name }, { defaultCacheConfig(ofSeconds(it.expiry)) })
 
 }
